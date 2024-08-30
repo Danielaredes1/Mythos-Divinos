db22b58bc7bfcf958beb8b9916b6f2ff123b9dc5
Davi Ramos <rdavi6558@gmail.com>
2024-08-29 22:32:16 -0300

    Add files via upload

---
+++ sidebar.js
@@ -0,0 +1,26 @@
+var header = document.getElementById('header');
+var nav = document.getElementById('pags');
+var content = document.getElementById('titulo');
+var showSidebar = false;
+
+function toggleSidebar() 
+{
+    showSidebar = !showSidebar;
+    if (showSidebar) 
+    {
+        nav.style.marginLeft = '76vw';
+        nav.style.animationName = 'sidebar';
+        
+    } 
+    else 
+    {
+        nav.style.marginLeft = '100vw';
+        nav.style.animationName = '';
+    }
+}
+
+function closeSidebar() {
+    if(showSidebar) {
+        toggleSidebar();
+    }
+}
